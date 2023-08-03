633c7da2846deca654afd6fef003c27bbee4cd1a
JuliSutiagina <juli.sutiagina@yandex.ru>
2023-08-03 12:40:41 +0300

    Добавлен README.md и комментарии в программу

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 ﻿Console.WriteLine("Введите длину массива");
-int len_arr_init = int.Parse(Console.ReadLine()!);
-string[] arr_init = new string[len_arr_init];
-int len_of_element_to_check = 3;
-string elements = "";
+int len_arr_init = int.Parse(Console.ReadLine()!); // Вводим длину массива с клавиатуры
+string[] arr_init = new string[len_arr_init]; // Создаем массив указанной длины
+int len_of_element_to_check = 3; // Задаем эталонную длину элементов
+string elements = ""; // Строка для конкатенации элементов длиной <= 3 символов
 for (int i = 0; i < len_arr_init; i++){
     Console.Write($"Введите строку для {i}-го элемента массива: ");
     arr_init[i] = Console.ReadLine()!;
